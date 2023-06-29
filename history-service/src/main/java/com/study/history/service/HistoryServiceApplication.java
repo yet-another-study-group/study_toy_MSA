1cc713bc67e118f546e35738422800dee10e81e5
Heoyungyoung <gjdbs2597@naver.com>
2023-06-29 23:57:28 +0900

    - history service 환경 세팅

---
+++ history-service/src/main/java/com/study/history/service/HistoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.study.history.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoryServiceApplication.class, args);
+	}
+
+}
