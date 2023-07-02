69017b2e6c6d218691d3ecb5dad64c00cf9091cc
최재영 <107467750+220SRS@users.noreply.github.com>
2023-07-02 17:42:03 +0900

    feat: book service server setting (#3)

---
+++ Book-Service/src/test/java/com/study/Book/Service/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.Book.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
