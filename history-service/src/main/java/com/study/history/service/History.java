8e4226e6f55c2474574d90e795a597e30138a1f5
Younyoung Heo <112863029+Heo-y-y@users.noreply.github.com>
2023-07-03 22:39:45 +0900

    [feat] History Entity 생성 (#11)
    * History Entity 생성
    
    * Entity 수정

---
+++ history-service/src/main/java/com/study/history/service/History.java
@@ -0,0 +1,15 @@
+package com.study.history.service;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
+public class History {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private long memberId;
+    private long bookId;
+    private int quantity;
+}
