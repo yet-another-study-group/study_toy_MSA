33d6115ace0a91f9f14137bd845d937a6af6df59
220SRS <107467750+220SRS@users.noreply.github.com>
2023-07-07 12:33:25 +0900

    feat: Loan Service init

---
+++ Loan-Service/src/test/java/com/study/Loan/Service/LoanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.Loan.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
