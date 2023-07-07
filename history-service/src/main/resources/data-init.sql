7726c281e3f07ed542a31df9ac4785a281773c7c
Heoyungyoung <gjdbs2597@naver.com>
2023-07-07 18:10:44 +0900

    data 변경

--- history-service/src/main/resources/data-init.sql
+++ history-service/src/main/resources/data-init.sql
@@ -5,7 +5,7 @@ INSERT INTO history (member_id, book_id, quantity)
 VALUE (1, 2, 2);
 
 INSERT INTO history (member_id, book_id, quantity)
-VALUE (1, 3, 3);
+VALUE (2, 2, 1);
 
 INSERT INTO history (member_id, book_id, quantity)
-VALUE (2, 1, 2);
+VALUE (2, 1, 1);
