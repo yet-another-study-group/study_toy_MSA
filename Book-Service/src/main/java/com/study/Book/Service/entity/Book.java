5ccb373765cda89cf72b9f716cc58acd05ff63f0
Heoyungyoung <gjdbs2597@naver.com>
2023-07-31 17:32:11 +0900

    test: BookAPI 테스트코드 작성

--- Book-Service/src/main/java/com/study/Book/Service/entity/Book.java
+++ Book-Service/src/main/java/com/study/Book/Service/entity/Book.java
@@ -1,6 +1,7 @@
 package com.study.Book.Service.entity;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +10,7 @@ import javax.persistence.Id;
 
 @Entity
 @Getter
+@Setter
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- Book-Service/src/test/java/com/study/Book/Service/BookServiceTest.java
+++ Book-Service/src/test/java/com/study/Book/Service/BookServiceTest.java
@@ -1,7 +1,11 @@
 package com.study.Book.Service;
 
+import com.study.Book.Service.entity.Book;
 import com.study.Book.Service.feign.HistoryFeign;
 import com.study.Book.Service.repository.BookRepository;
+import com.study.Book.Service.response.BookAvailabilityStatus;
+import com.study.Book.Service.response.BookStockRecord;
+import com.study.Book.Service.response.BookStockResponse;
 import com.study.Book.Service.service.BookService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,67 +15,103 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class BookServiceTest {
-
     @InjectMocks
     BookService bookService;
-
     @Mock
     BookRepository bookRepository;
-
     @Mock
     HistoryFeign historyFeign;
 
     @Test
-    @DisplayName("책 대여 가능 여부 성공")
-    void test1() {
-        when(historyFeign.getBorrowedBookCount(anyLong())).thenReturn(2);
-        when(bookRepository.findStockById(anyLong())).thenReturn(5);
-        when(bookRepository.existsById(anyLong())).thenReturn(true);
+    @DisplayName("책 대여가 가능한 경우")
+    public void testBookAvailable() {
+        // given
+        long bookId = 1L;
+        int stock = 5;
+        int quantity = 2;
 
-        boolean pass = bookService.verify(anyLong());
+        Book book = new Book();
+        book.setId(bookId);
+        book.setStock(stock);
 
-        assertThat(pass).isEqualTo(true);
+        List<BookStockRecord> stockRecords = Collections.singletonList(new BookStockRecord(1L, quantity));
+
+        BookStockResponse stockResponse = mock(BookStockResponse.class);
+        when(stockResponse.getBookStockRecords()).thenReturn(stockRecords);
+        when(historyFeign.getBorrowedBookStock(bookId)).thenReturn(stockResponse);
+        when(bookRepository.findById(bookId)).thenReturn(java.util.Optional.of(book));
+        // when
+        BookAvailabilityStatus result = bookService.checkBookAvailabilityForRental(bookId);
+        // then
+        assertEquals(BookAvailabilityStatus.AVAILABLE, result);
+        verify(bookRepository).findById(bookId);
+        verify(historyFeign).getBorrowedBookStock(bookId);
     }
 
     @Test
-    @DisplayName("책 대여 가능 여부 성공2")
-    void test1_2() {
-        when(historyFeign.getBorrowedBookCount(anyLong())).thenReturn(4);
-        when(bookRepository.findStockById(anyLong())).thenReturn(5);
-        when(bookRepository.existsById(anyLong())).thenReturn(true);
+    @DisplayName("책 대여가 불가능한 경우")
+    public void testBookNotAvailable() {
+        // given
+        long bookId = 1L;
+        int stock = 5;
+        int quantity = 5;
 
-        boolean pass = bookService.verify(anyLong());
+        Book book = new Book();
+        book.setId(bookId);
+        book.setStock(stock);
 
-        assertThat(pass).isEqualTo(true);
+        List<BookStockRecord> stockRecords = Collections.singletonList(new BookStockRecord(1L, quantity));
+
+        BookStockResponse stockResponse = mock(BookStockResponse.class);
+        when(stockResponse.getBookStockRecords()).thenReturn(stockRecords);
+        when(historyFeign.getBorrowedBookStock(bookId)).thenReturn(stockResponse);
+        when(bookRepository.findById(bookId)).thenReturn(Optional.of(book));
+        // when
+        BookAvailabilityStatus result = bookService.checkBookAvailabilityForRental(bookId);
+        // then
+        assertEquals(BookAvailabilityStatus.NOT_AVAILABLE, result);
+        verify(bookRepository).findById(bookId);
+        verify(historyFeign).getBorrowedBookStock(bookId);
     }
 
     @Test
-    @DisplayName("책 대여 가능 여부 실패")
-    void test2() {
-        when(historyFeign.getBorrowedBookCount(anyLong())).thenReturn(5);
-        when(bookRepository.findStockById(anyLong())).thenReturn(5);
-        when(bookRepository.existsById(anyLong())).thenReturn(true);
+    @DisplayName("책의 재고를 반환하는 경우")
+    public void testGetBookStock() {
+        // given
+        long bookId = 1L;
+        int stock = 5;
 
-        boolean pass = bookService.verify(anyLong());
+        Book book = new Book();
+        book.setId(bookId);
+        book.setStock(stock);
 
-        assertThat(pass).isEqualTo(false);
+        when(bookRepository.findById(bookId)).thenReturn(Optional.of(book));
+        //when
+        int bookStock = bookService.getTotalBookStock(bookId);
+        //then
+        assertEquals(stock, bookStock);
+        verify(bookRepository).findById(bookId);
     }
 
     @Test
-    @DisplayName("존재하지 않는 BookId")
-    void test3() {
-        when(bookRepository.existsById(anyLong())).thenReturn(false);
-
+    @DisplayName("bookId를 못 찾는 경우 예외를 던지는지 확인")
+    public void testBookStockNotFound() {
+        // given
+        long nonExistentBookId = 9L;
+        when(bookRepository.findById(nonExistentBookId)).thenReturn(Optional.empty());
+        // when, then
         assertThrows(EntityNotFoundException.class, () -> {
-            bookService.verify(anyLong());
+            bookService.getTotalBookStock(nonExistentBookId);
         });
+        verify(bookRepository).findById(nonExistentBookId);
     }
 }
